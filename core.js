6da8db9098ca68d0f0b1bdf432a562747deb054b
Ash-86 <108089527+Ash-86@users.noreply.github.com>
2025-12-13 20:51:25 -0300

    add list selection

--- core.js
+++ core.js
@@ -15,7 +15,7 @@ function applyTransform(type, roll) {
     var endTrack = endStaff * 4;
     //start		
     cursor.rewind(1); // go to the beginning of the selection
-    var startSegTick= curScore.selection.startSegment.tick;
+    //var startSegTick= curScore.selection.startSegment.tick;
     var startTick = cursor.tick;
     var startStaff = cursor.staffIdx;
     var startTrack = startStaff * 4;
@@ -24,16 +24,19 @@ function applyTransform(type, roll) {
 
     curScore.startCmd()    
         
-        if (type == "melody") {
-            var allMelodies = getMelody()      
-            rollMelody(allMelodies, roll)
+    if (true ) {
+        rollListSel()
+
+    } else if (type == "melody") {
+        var allMelodies = getMelody()      
+        rollMelody(allMelodies, roll)
             
-        }
-        if (type == "chord") {
-            var allChords = getChords()
-            rollChord(allChords, roll)
-        }
-        curScore.selection.selectRange(startTick, endTick, startStaff, endStaff);
+    } else if (type == "chord") {
+        var allChords = getChords()
+        rollChord(allChords, roll)
+    }
+        
+    //curScore.selection.selectRange(startTick, endTick, startStaff, endStaff);
 
     curScore.endCmd()   
 
@@ -61,7 +64,91 @@ function applyTransform(type, roll) {
     
     
     /////////////////////////////////////////////////////////////   
-    
+    function rollListSel() {
+        var els = curScore.selection.elements        
+        
+        var notes = [];
+        for (var i = 0; i < els.length; i++) { //change elements array to a js array to operate on by sort/filter ...
+            if (els[i].pitch != undefined) {
+                notes.push(els[i])
+            }
+        }
+
+        //notes = notes.filter(e => e.type == Element.NOTE)
+        //var elsByTick = notes.sort((a, b) => a.tick < b.tick)
+        //var elsByTrack = notes.sort((a, b) => a.track < b.track)
+        
+        var byTrack = {}
+        var byTick = {}
+        for (var i = 0; i < notes.length; i++) {
+            var track = notes[i].track
+            var tick = notes[i].parent.parent.tick
+            if (!byTrack[track]) {
+                byTrack[track] = []
+            }
+            if (!byTick[tick]) {
+                byTick[tick] = []
+            }
+            byTrack[track].push(notes[i])
+            byTick[tick].push(notes[i])
+        }
+
+        if (type == "melody") rollByAttribute(byTrack) 
+        if (type == "chord") rollByAttribute(byTick)
+        
+        function rollByAttribute(byAttribute) {
+            for (var att in byAttribute) {
+                var notesOfAtt = byAttribute[att]                
+
+                var primes = []
+                for (var m = 0; m < notesOfAtt.length; m++) {
+                        
+                    var prime = {
+                        pitch: notesOfAtt[m].pitch % 12,
+                        tpc1: notesOfAtt[m].tpc1,
+                        tpc2: notesOfAtt[m].tpc2
+                    }
+
+                    if (!primes.some(function (x) { return x.pitch == prime.pitch })) {        ////discard note is pitch already exists                        
+                        primes.push(prime)
+                    }
+                }
+                primes.sort(function (a, b) { return a.pitch - b.pitch }) //sorting up
+
+                var scale = []
+                for (var k = 0; k < 12; k++) {
+                    for (var m = 0; m < primes.length; m++) {
+                        var pitch = {
+                            pitch: primes[m].pitch + 12 * k,
+                            tpc1: primes[m].tpc1,
+                            tpc2: primes[m].tpc2
+                        }
+                        scale.push(pitch)
+                    }
+                }
+                var direction = roll == "up" ? 1 : -1
+                for (var m = 0; m < notesOfAtt.length; m++) {
+                    var idx = scale.findIndex(function (p) { return p.pitch == notesOfAtt[m].pitch })
+                    notesOfAtt[m].pitch = scale[idx + direction].pitch
+                    notesOfAtt[m].tpc1 = scale[idx + direction].tpc1
+                    notesOfAtt[m].tpc2 = scale[idx + direction].tpc2
+                }
+            }
+        }
+
+        
+
+        // function rollByAttribute(byAttribute) { 
+        //     for (var attribute in byAttribute) {
+        //         var notesPerAttribute = byAttribute[attribute]
+        //         //console.log("attribute:", attribute, "Notes:", notesPerAttribute)
+        //         var scale = getOrderedSet(notesPerAttribute)
+        //         for (var n in notesPerTAttribute) {
+        //             replaceElement(n, scale, roll)
+        //         }
+        //     }
+        // }
+    }
 
     function getMelody() {
         
@@ -105,12 +192,13 @@ function applyTransform(type, roll) {
         var i = 0
         cursor.rewind(1)
         while (cursor.segment != null && cursor.tick < endTick) {  
-            var C = getOrderedSet(allChords[i])
+            var scale = getOrderedSet(allChords[i])
             
             for(var track = startTrack; track < endTrack; track++){
                 cursor.track = track
                 if (!cursor.element) continue
-                replaceElement(C, roll)                
+                var el = cursor.element
+                replaceElement(el, scale, roll)                
             }                    
             i++ 
             cursor.track = startTrack 
@@ -127,9 +215,10 @@ function applyTransform(type, roll) {
             if (!cursor.element) {
                 continue
             }
-            var C = getOrderedSet(allMelodies[i])
+            var scale = getOrderedSet(allMelodies[i])
             while (cursor.segment != null && cursor.tick < endTick) {
-                replaceElement(C, roll)
+                var el = cursor.element
+                replaceElement(el, scale, roll)
                 cursor.next()
             }             
             i++
@@ -139,8 +228,8 @@ function applyTransform(type, roll) {
 
     //////////////////////////////////////////////////////////
 
-    function replaceElement(set, roll) { 
-        var el = cursor.element
+    function replaceElement(el, set, roll) { 
+        
         if (el.type == Element.CHORD) {
         
             if (roll == "up") {
@@ -163,6 +252,21 @@ function applyTransform(type, roll) {
                 }
             }
             
+        }
+        if (el.type == Element.NOTE) {
+            
+            if (roll == "up") {
+                var idx = set.findIndex(function (obj) { return obj.pitch == el.pitch });
+                el.pitch = set[idx + 1].pitch
+                el.tpc1 = set[idx + 1].tpc1
+                el.tpc2 = set[idx + 1].tpc2
+            }
+            if (roll == "down") {
+                var idx = set.findIndex(function (obj) { return obj.pitch == el.pitch });
+                el.pitch = set[idx - 1].pitch
+                el.tpc1 = set[idx - 1].tpc1
+                el.tpc2 = set[idx - 1].tpc2
+            }
         }
     }
 
